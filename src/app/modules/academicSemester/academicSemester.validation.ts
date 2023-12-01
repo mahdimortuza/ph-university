dd88ee4f91860088b2fb11a0256e6247ca43efc1
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-01 18:55:30 +0600

    validations done

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -1,12 +1,18 @@
 import z from 'zod';
+import {
+  AcademicSemesterCode,
+  AcademicSemesterName,
+  Months,
+} from './academicSemester.constants';
 
 const createAcademicSemesterValidationSchema = z.object({
-  password: z
-    .string({
-      invalid_type_error: 'Password must be string',
-    })
-    .max(20, { message: 'password must be less than 20  characters' })
-    .optional(),
+  body: z.object({
+    name: z.enum([...(AcademicSemesterName as [string, ...string[]])]),
+    year: z.date(),
+    code: z.enum([...(AcademicSemesterCode as [string, ...string[]])]),
+    startMonth: z.enum([...(Months as [string, ...string[]])]),
+    endMonth: z.enum([...(Months as [string, ...string[]])]),
+  }),
 });
 
 export const AcademicSemesterValidations = {
