526574d867b1add01d2423ec1283cd259d9ed6c4
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-27 14:18:40 +0600

    password update done

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -15,16 +15,15 @@ const loginUser = catchAsync(async (req, res) => {
 });
 
 const changePassword = catchAsync(async (req, res) => {
-  console.log(req.user, req.body);
   const user = req.user;
   const { ...passwordData } = req.body;
 
-  const result = await AuthServices.changePassword(user, passwordData);
+  const result = await AuthServices.changePassword(req.user, passwordData);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'user is logged in successfully',
+    message: 'password is updated successfully',
     data: result,
   });
 });
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,5 +1,6 @@
+import bcrypt from 'bcrypt';
 import httpStatus from 'http-status';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../../config';
 import { AppError } from '../../errors/AppError';
 import { User } from '../user/user.model';
@@ -7,7 +8,6 @@ import { TLoginUser } from './auth.interface';
 
 const loginUser = async (payload: TLoginUser) => {
   // checking if the user exists
-
   const user = await User.isUserExistsByCustomId(payload.id);
   console.log(user);
 
@@ -50,11 +50,54 @@ const loginUser = async (payload: TLoginUser) => {
   };
 };
 
-const changePassword = (user: { userId: string; role: string }, payload) => {
-  const result = await User.findOneAndUpdate({
-    id: user.userId,
-    role: user.role,
-  });
+const changePassword = async (
+  userData: JwtPayload,
+  payload: { oldPassword: string; newPassword: string },
+) => {
+  // checking if the user exists
+  const user = await User.isUserExistsByCustomId(userData.userId);
+  console.log(user);
+
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'this user is not found!');
+  }
+
+  // checking if the user is already deleted
+  const isDeleted = user?.isDeleted;
+
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
+  }
+
+  // checking if the user is already blocked
+  const userStatus = user?.status;
+
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked ! !');
+  }
+
+  // checking if the password is correct
+  if (!(await User.isPasswordMatched(payload.oldPassword, user?.password)))
+    throw new AppError(httpStatus.FORBIDDEN, 'Password do not matched');
+
+  // hash new password
+  const newHashedPassword = await bcrypt.hash(
+    payload.newPassword,
+    Number(config.bcrypt_salt_round),
+  );
+
+  const result = await User.findOneAndUpdate(
+    {
+      id: userData.userId,
+      role: userData.role,
+    },
+    {
+      password: newHashedPassword,
+      needsPasswordChange: false,
+      passwordChangedAt: new Date(),
+    },
+  );
+  return null;
 };
 
 export const AuthServices = {
