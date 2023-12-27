f61856c212820883631f2b76ad6314e0a0aa06d4
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-27 11:25:03 +0600

    authotization added

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,12 +2,12 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { AppError } from '../errors/AppError';
+import { TUserRole } from '../modules/user/user.interface';
 import catchAsync from '../utils/catchAsync';
 
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
-
     // checking if the token is sent from the client
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
@@ -24,6 +24,15 @@ const auth = () => {
             'You are not authorized!',
           );
         }
+        const role = (decoded as JwtPayload)?.role;
+
+        if (requiredRoles && !requiredRoles.includes(role)) {
+          throw new AppError(
+            httpStatus.UNAUTHORIZED,
+            'You are not authorized!',
+          );
+        }
+
         // decoded undefined
         req.user = decoded as JwtPayload;
         next();
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,13 +3,14 @@ import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { createAdminValidationSchema } from '../admin/admin.validation';
 import { createStudentValidationSchema } from '../student/student.validation';
+import { USER_ROLE } from './user.constant';
 import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
 router.post(
   '/create-student',
-  auth(),
+  auth(USER_ROLE.admin),
   validateRequest(createStudentValidationSchema),
   UserControllers.createStudent,
 );
