92ba58822103b47262cb6b889c1ce03e00cc0736
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-30 00:44:16 +0600

    student enrolled

--- src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -1,4 +1,5 @@
 import httpStatus from 'http-status';
+import mongoose from 'mongoose';
 import { AppError } from '../../errors/AppError';
 import { OfferedCourse } from '../offeredCourse/offeredCourse.model';
 import { Student } from '../student/student.model';
@@ -43,27 +44,50 @@ const createEnrolledCourseIntoDB = async (
     throw new AppError(httpStatus.CONFLICT, 'student is already enrolled.');
   }
 
-  const result = await EnrolledCourse.create({
-    semesterRegistration: isOfferedCourseExists.semesterRegistration,
-    academicSemester: isOfferedCourseExists.academicSemester,
-    academicFaculty: isOfferedCourseExists.academicFaculty,
-    academicDepartment: isOfferedCourseExists.academicDepartment,
-    offeredCourse: offeredCourse,
-    course: isOfferedCourseExists.course,
-    student: student._id,
-    faculty: isOfferedCourseExists.faculty,
-  });
+  const session = await mongoose.startSession();
 
-  if (!result) {
-    throw new AppError(httpStatus.CONFLICT, 'failed to enroll in this course.');
+  try {
+    session.startTransaction();
+
+    const result = await EnrolledCourse.create(
+      [
+        {
+          semesterRegistration: isOfferedCourseExists.semesterRegistration,
+          academicSemester: isOfferedCourseExists.academicSemester,
+          academicFaculty: isOfferedCourseExists.academicFaculty,
+          academicDepartment: isOfferedCourseExists.academicDepartment,
+          offeredCourse: offeredCourse,
+          course: isOfferedCourseExists.course,
+          student: student._id,
+          faculty: isOfferedCourseExists.faculty,
+          isEnrolled: true,
+        },
+      ],
+      { session },
+    );
+
+    if (!result) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        'failed to enroll in this course.',
+      );
+    }
+
+    const maxCapacity = isOfferedCourseExists.maxCapacity;
+    await OfferedCourse.findByIdAndUpdate(offeredCourse, {
+      maxCapacity: maxCapacity - 1,
+    });
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return result;
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+
+    throw new Error(err);
   }
-
-  const maxCapacity = isOfferedCourseExists.maxCapacity;
-  await OfferedCourse.findByIdAndUpdate(offeredCourse, {
-    maxCapacity: maxCapacity - 1,
-  });
-
-  return result;
 };
 
 export const EnrolledCourseServices = {
